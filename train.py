52c8af554058ffb1037b6e156906b65dddec5e7b
mvsakrishna <73127269+mvsakrishna@users.noreply.github.com>
2024-04-16 13:11:28 -0400

    Update train.py

--- train.py
+++ train.py
@@ -93,9 +93,9 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
             
             # Print the source, target and model output
             print_msg('-'*console_width)
-            print_msg(f"{f'SOURCE: ':>12}{source_text}")
-            print_msg(f"{f'TARGET: ':>12}{target_text}")
-            print_msg(f"{f'PREDICTED: ':>12}{model_out_text}")
+            # print_msg(f"{f'SOURCE: ':>12}{source_text}")
+            # print_msg(f"{f'TARGET: ':>12}{target_text}")
+            # print_msg(f"{f'PREDICTED: ':>12}{model_out_text}")
 
             if count == num_examples:
                 print_msg('-'*console_width)
